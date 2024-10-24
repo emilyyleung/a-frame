1b6b9dd61a590634936a2be11b110fc06744dd33
Emily Y Leung <30677901+emilyyleung@users.noreply.github.com>
2024-10-24 21:16:52 +1100

    Add deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://emilyyleung.github.io",
+  base: "a-frame",
   integrations: [react(), tailwind({ applyBaseStyles: false })],
 });
